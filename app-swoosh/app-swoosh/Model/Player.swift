a9d2ce21f6fc2995975faff752d86222fe708b10
Andrew Mostert <bull@andrewmostert.com>
2018-08-11 20:49:13 +0200

    Final app

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andrew Mostert on 2018/08/11.
+//  Copyright © 2018 Andrew Mostert. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
